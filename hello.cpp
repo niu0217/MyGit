5fbab1fc91f6f439ba7b7bf031dbb8f30c7e7686
niu0217 <1033856427@qq.com>
2024-04-10 16:54:24 +0800

    成功调试C++代码

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main()
 {
+    int a = 0;
+    int b = 12;
+    int c = 13;
+    int d = 14;
     cout << "hello world" << endl;
     cout << "nihao" << endl;
+    cout << d << endl;
 }

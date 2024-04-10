61f4025fa957e1b39c3d5342f34bdfc018415116
niu0217 <1033856427@qq.com>
2024-04-10 20:01:32 +0800

    修改hello的代码

--- hello.cpp
+++ hello.cpp
@@ -7,6 +7,8 @@ int main()
     int b = 12;
     int c = 13;
     int d = 14;
+    cin >> a;
+    cout << a << endl;
     cout << "hello world" << endl;
     cout << "nihao" << endl;
     cout << d << endl;
